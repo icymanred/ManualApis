77f82090c60c2eaae2544ea53d96d25e32a81440
icymanred <71398492+icymanred@users.noreply.github.com>
2024-02-19 17:05:29 +0000

    Update terminateProcess.asm

--- src/ntdll/ProcessThreadsapi/terminateProcess.asm
+++ src/ntdll/ProcessThreadsapi/terminateProcess.asm
@@ -5,8 +5,8 @@ global _main
 	section .text
 	
 _main:
-	mov r10,0xFFFFFFFFFFFFFFFF
-	mov eax,0x0000002C
-	mov rdx,0x90
+	mov r10,0xFFFFFFFFFFFFFFFF ; Constant to current process pseudo handle
+	mov eax,0x0000002C ; syscall number
+	mov rdx,0x90 ; Change exit code
 	syscall
 	
